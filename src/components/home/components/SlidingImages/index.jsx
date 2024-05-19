6bfb4c817070e874d66a240dbc14d8e0c3bf791a
‘Trey <treyrdr09@gmail.com>
2024-05-19 18:34:48 -0500

    refactored sliding images

--- src/components/home/components/SlidingImages/index.jsx
+++ src/components/home/components/SlidingImages/index.jsx
@@ -3,8 +3,8 @@
 import { useRef } from "react";
 import { useScroll, useTransform, motion } from "framer-motion";
 import Image from "next/image";
-import styles from "./styles";
 import { slider1, slider2 } from "./sliders";
+import { cn } from "@/lib/utils";
 
 // const imageProps = {
 //   fill,
@@ -12,6 +12,36 @@ import { slider1, slider2 } from "./sliders";
 //   className: "object-cover",
 //   sizes: {}
 // };
+
+const styles = {
+  slidingImages: cn(
+    "flex",
+    "flex-col",
+    "gap-[3vw]",
+    "relative",
+    "mt-[200px]",
+    "bg-white",
+    "z-[1]"
+  ),
+  slider: cn("flex", "relative", "gap-[3vw]", "w-[120vw]", "-left-[10vw]"),
+  project: cn("w-[25%]", "h-[20vw]", "flex", "items-center", "justify-center"),
+  imageContainer: cn("relative", "w-[80%]", "h-[80%]"),
+  circleContainer: cn("bg-red-500", "relative", "mt-[100px]"),
+  circle: cn(
+    "h-[1550%]",
+    "w-[120%]",
+    "-left-[10%]",
+    "rounded-tl-none",
+    "rounded-tr-none",
+    "rounded-bl-full",
+    "rounded-br-full",
+    "bg-white",
+    "z-[1]",
+    "absolute",
+    // "shadow-lg",
+    "custom-shadow"
+  ),
+};
 
 export default function SlidingImages() {
   const container = useRef(null);
@@ -25,16 +55,42 @@ export default function SlidingImages() {
   const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
 
   return (
-    <div ref={container} className={styles.slidingImages}>
-      <motion.div style={{ x: x1 }} className={styles.slider}>
+    <div
+      ref={container}
+      className={cn(
+        "flex",
+        "flex-col",
+        "gap-[3vw]",
+        "relative",
+        "mt-[200px]",
+        "bg-white",
+        "z-[1]"
+      )}
+    >
+      <motion.div
+        style={{ x: x1 }}
+        className={cn(
+          "flex",
+          "relative",
+          "gap-[3vw]",
+          "w-[120vw]",
+          "-left-[10vw]"
+        )}
+      >
         {slider1.map((project, index) => {
           return (
             <div
               key={index}
-              className={styles.project}
+              className={cn(
+                "w-[25%]",
+                "h-[20vw]",
+                "flex",
+                "items-center",
+                "justify-center"
+              )}
               style={{ backgroundColor: project.color }}
             >
-              <div className={styles.imageContainer}>
+              <div className={cn("relative size-4/5")}>
                 <Image
                   fill
                   alt="image"
@@ -47,18 +103,33 @@ export default function SlidingImages() {
           );
         })}
       </motion.div>
-      <motion.div style={{ x: x2 }} className={styles.slider}>
+      <motion.div
+        style={{ x: x2 }}
+        className={cn(
+          "flex",
+          "relative",
+          "gap-[3vw]",
+          "w-[120vw]",
+          "-left-[10vw]"
+        )}
+      >
         {slider2.map((project, index) => {
           return (
             <div
               key={index}
-              className={styles.project}
+              className={cn(
+                "w-[25%]",
+                "h-[20vw]",
+                "flex",
+                "items-center",
+                "justify-center"
+              )}
               style={{ backgroundColor: project.color }}
             >
-              <div key={index} className={styles.imageContainer}>
+              <div key={index} className={cn("relative size-4/5")}>
                 <Image
                   fill={true}
-                  alt={"image"}
+                  alt="image"
                   src={`/images/${project.src}`}
                   priority={project.isPriority}
                   sizes={{}}
@@ -68,8 +139,27 @@ export default function SlidingImages() {
           );
         })}
       </motion.div>
-      <motion.div style={{ height }} className={styles.circleContainer}>
-        <div className={styles.circle} />
+
+      <motion.div
+        style={{ height }}
+        className={cn("bg-red-500 relative mt-[100px]")}
+      >
+        <div
+          className={cn(
+            "h-[1550%]",
+            "w-[120%]",
+            "-left-[10%]",
+            "rounded-tl-none",
+            "rounded-tr-none",
+            "rounded-bl-full",
+            "rounded-br-full",
+            "bg-white",
+            "z-[1]",
+            "absolute",
+            // "shadow-lg",
+            "custom-shadow"
+          )}
+        />
       </motion.div>
     </div>
   );
