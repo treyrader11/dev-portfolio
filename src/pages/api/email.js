ea71a795f00448a62656832a2f84ba0c93c9b8d2
‘Trey <treyrdr09@gmail.com>
2024-07-01 16:32:40 -0500

    deleted method check

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -7,9 +7,9 @@ const WaitlistEmail = require("@/lib/emails/waitlist");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  // if (req.method !== "POST") {
+  //   return res.status(405).json({ error: "Method not allowed" });
+  // }
 
   const { name, email, subject, message } = req.body;
   console.log(
