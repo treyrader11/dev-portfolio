323b9b2d96dacada7c9dccb050e44d5b11649d18
‘Trey <treyrdr09@gmail.com>
2024-07-01 18:15:58 -0500

    fetched with axios

--- src/components/Contact/components/ContactForm/index.jsx
+++ src/components/Contact/components/ContactForm/index.jsx
@@ -14,6 +14,7 @@ import PaperPlane from "../PaperPlane";
 import { getPlaneKeyframes } from "../../getPlaneKeyframes";
 import { getTrailsKeyframes } from "../../getTrailsKeyframes";
 import SubmitEmail from "../SubmitEmail";
+import axios from "axios";
 
 const initValues = { name: "", email: "", subject: "", message: "" };
 
@@ -43,70 +44,126 @@ export default function ContactForm({ className }) {
       },
     }));
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // setState((prev) => ({
-    //   ...prev,
-    //   isLoading: true,
-    // }));
-
-    const { name, email, subject, message } = values;
-    // const email = input;
-
-    console.log("email in handle submit", email);
-    const button = buttonRef.current;
-
-    // if (!validateEmail(email) || name || subject || message || !button) return;
-    if (!email || !button) return;
-
-    if (!isActive) {
-      setIsActive(true);
-
-      to(button, {
-        keyframes: getPlaneKeyframes(
-          set,
-          fromTo,
-          button,
-          setIsActive,
-          setInput,
-          setState,
-          initState
-        ),
-      });
-
-      to(button, { keyframes: getTrailsKeyframes(button) });
-    }
-
-    try {
-      const res = await fetch("/api/email", {
-        body: JSON.stringify({ name, email, subject, message }),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-      });
-
-      console.log("Response from API:", res);
-
-      if (!res.ok) {
-        const errorData = await res.json();
-        console.error("Error response from API:", errorData);
-        setErrorMessage(`Something went wrong! Details: "${errorData.error}"`);
-        setSuccessMessage(undefined);
-        return;
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      const { name, email, subject, message } = values;
+  
+      console.log("email in handle submit", email);
+      const button = buttonRef.current;
+  
+      if (!email || !button) return;
+  
+      if (!isActive) {
+        setIsActive(true);
+  
+        to(button, {
+          keyframes: getPlaneKeyframes(
+            set,
+            fromTo,
+            button,
+            setIsActive,
+            setInput,
+            setState,
+            initState
+          ),
+        });
+  
+        to(button, { keyframes: getTrailsKeyframes(button) });
       }
+  
+      try {
+        const res = await axios.post("/api/email", {
+          name,
+          email,
+          subject,
+          message,
+        });
+  
+        console.log("Response from API:", res);
+  
+        if (res.status !== 200) {
+          console.error("Error response from API:", res.data);
+          setErrorMessage(`Something went wrong! Details: "${res.data.error}"`);
+          setSuccessMessage(undefined);
+          return;
+        }
+  
+        console.log("Successful response from API:", res.data);
+  
+        setSuccessMessage("Thank you for getting in touch!");
+        setErrorMessage(undefined);
+      } catch (error) {
+        console.error("Error making API request:", error);
+        setErrorMessage(`Something went wrong! Details: "${error.message}"`);
+        setSuccessMessage(undefined);
+      }
+    };
 
-      const data = await res.json();
-      console.log("Successful response from API:", data);
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
 
-      // setSuccessMessage(data.res);
-      setSuccessMessage("Thank you for getting in touch!");
-      setErrorMessage(undefined);
-    } catch (error) {
-      console.error("Error making API request:", error);
-      setErrorMessage(`Something went wrong! Details: "${error}"`);
-      setSuccessMessage(undefined);
-    }
-  };
+  //   // setState((prev) => ({
+  //   //   ...prev,
+  //   //   isLoading: true,
+  //   // }));
+
+  //   const { name, email, subject, message } = values;
+  //   // const email = input;
+
+  //   console.log("email in handle submit", email);
+  //   const button = buttonRef.current;
+
+  //   // if (!validateEmail(email) || name || subject || message || !button) return;
+  //   if (!email || !button) return;
+
+  //   if (!isActive) {
+  //     setIsActive(true);
+
+  //     to(button, {
+  //       keyframes: getPlaneKeyframes(
+  //         set,
+  //         fromTo,
+  //         button,
+  //         setIsActive,
+  //         setInput,
+  //         setState,
+  //         initState
+  //       ),
+  //     });
+
+  //     to(button, { keyframes: getTrailsKeyframes(button) });
+  //   }
+
+  //   try {
+  //     const res = await fetch("/api/email", {
+  //       body: JSON.stringify({ name, email, subject, message }),
+  //       headers: { "Content-Type": "application/json" },
+  //       method: "POST",
+  //     });
+
+  //     console.log("Response from API:", res);
+
+  //     if (!res.ok) {
+  //       const errorData = await res.json();
+  //       console.error("Error response from API:", errorData);
+  //       setErrorMessage(`Something went wrong! Details: "${errorData.error}"`);
+  //       setSuccessMessage(undefined);
+  //       return;
+  //     }
+
+  //     const data = await res.json();
+  //     console.log("Successful response from API:", data);
+
+  //     // setSuccessMessage(data.res);
+  //     setSuccessMessage("Thank you for getting in touch!");
+  //     setErrorMessage(undefined);
+  //   } catch (error) {
+  //     console.error("Error making API request:", error);
+  //     setErrorMessage(`Something went wrong! Details: "${error}"`);
+  //     setSuccessMessage(undefined);
+  //   }
+  // };
 
   const dismissMessages = () => {
     setSuccessMessage(undefined);
